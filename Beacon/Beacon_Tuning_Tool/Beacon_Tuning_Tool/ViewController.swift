cd8fed2c76afe0fc08b54dfa06313cc69f13b810
scropio <scropio.neil@gmail.com>
2015-09-15 20:22:00 +0800

    Initial Commit

---
+++ Beacon/Beacon_Tuning_Tool/Beacon_Tuning_Tool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beacon_Tuning_Tool
+//
+//  Created by Neil on 2015/9/15.
+//  Copyright (c) 2015年 Vincere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
